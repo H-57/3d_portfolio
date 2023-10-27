2f4c9caf47ff2fece6f78b3f7b557535746583ab
tarun <tarunkumar8059@gmail.com>
2023-10-27 03:36:30 -0700

    Revert "api for cards"
    This reverts commit a1ec15c7737623f13a0633e2c37764f9722628ae.

--- src/app/api/experienceCards/route.ts
+++ src/app/api/experienceCards/route.ts
@@ -1,28 +1,10 @@
 import { NextResponse } from "next/server";
 import { dbConnection } from "../db/connection";
-import { ExperienceCard } from "../models/experienceCard";
-
-interface bodyProps{
-    projectName:String,
-    company:String,
-    date:String,
-    points:[String]
-}
 
 
 export async function GET(request: Request) {
-  dbConnection();
-const result=await ExperienceCard.find({})
-  return NextResponse.json(result);
-}
-
-export async function Post(request: Request) {
- dbConnection();
- if(!request.body){
-    return NextResponse
- }
- const data:bodyProps=request.body;
- console.log(data);
-  const result=await ExperienceCard.create(data)
   
+    dbConnection()
+  
+    return NextResponse.json({})
 }
