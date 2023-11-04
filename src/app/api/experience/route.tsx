50a30b0638b54bcce422e0a9830d21efabe67caf
tarun <tarunkumar8059@gmail.com>
2023-11-04 07:03:52 -0700

    test

--- src/app/api/experience/route.tsx
+++ src/app/api/experience/route.tsx
@@ -40,7 +40,7 @@ export async function POST(request:Request) {
   return NextResponse.json({ message: "created",result },{ status: 201});
   }
   } catch (error) {
-    return NextResponse.json({ error},{ status: 500});
+    return NextResponse.json({error: error},{ status: 400});
   }
   
 
