9bcdf9e336f16760e06b6296504dbb51367511d5
tarun <tarunkumar8059@gmail.com>
2023-11-10 06:36:38 -0800

    fix mongodb

--- src/app/utils/db/connection.tsx
+++ src/app/utils/db/connection.tsx
@@ -1,11 +1,6 @@
-import _mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 
-declare global {
-  var mongoose: {
-    promise: ReturnType<typeof connect> | null;
-    conn: typeof _mongoose | null;
-  };
-}
+
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -13,30 +8,12 @@ if (!MONGODB_URI || MONGODB_URI.length === 0) {
   throw new Error("Please add your MongoDB URI to .env.local");
 }
 
-/**
- * Global is used here to maintain a cached connection across hot reloads
- * in development. This prevents connections from growing exponentially
- * during API Route usage.
- */
-let cached = global.mongoose;
 
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
 
 export async function dbConnection() {
-  if (cached.conn) {
-    console.log("🚀 Using cached connection");
-    return cached.conn;
-  }
+  
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = connect(MONGODB_URI!, opts)
-      .then((mongoose) => {
+mongoose.connect(MONGODB_URI!).then((mongoose) => {
         console.log("✅ New connection established");
         return mongoose;
       })
@@ -46,14 +23,9 @@ export async function dbConnection() {
       });
   }
 
-  try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
-  }
 
-  return cached.conn;
-}
+
+  
+
 
 
